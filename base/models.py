eb3fcb17ee95bd0f3940092781cac46dc77521e5
akshay-srini <akshayerode@gmail.com>
2025-01-12 19:32:33 +0530

    modified the user position from charfield to choicefield, and introduced a new model called leaves

--- base/models.py
+++ base/models.py
@@ -3,7 +3,30 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CompanyUser(AbstractUser):
-    position = models.CharField(max_length=150)
+    position_choices = [
+        ('employee', 'employee'),
+        ('manager', 'manager')
+    ]
+    position = models.CharField(max_length=100, choices=position_choices, default='employee')
 
     def __str__(self):
-         return f"{self.user.username} - {self.position}"
+        return f"{self.user.username} - {self.position}"
+
+class Leaves(models.Model):
+    leave_type_choices = [
+        ('casual', 'casual'),
+        ('sick', 'sick')
+    ]
+    leave_status_choices = [
+        ('pending', 'pending'),
+        ('successful', 'successful'),
+        ('rejected', 'rejected')
+    ]
+    employee = models.ForeignKey(CompanyUser, on_delete=models.CASCADE)
+    no_of_days = models.IntegerField(default=0, null=False)
+    leave_type = models.CharField(choices=leave_type_choices, default='casual', max_length=100)
+    reason = models.CharField(blank=True, null=True, max_length=500)
+    status = models.CharField(choices=leave_type_choices, default='pending', max_length=100)
+
+    def __str__(self):
+        return super().__str__()
